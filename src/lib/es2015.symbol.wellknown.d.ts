6c9c073295185d9b9ddee7211e6b5c948452dad1
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2021-01-31 16:30:00 +0100

    Fix definition of `Array.prototype[Symbol.unscopables]`

--- src/lib/es2015.symbol.wellknown.d.ts
+++ src/lib/es2015.symbol.wellknown.d.ts
@@ -73,17 +73,21 @@ interface Symbol {
 
 interface Array<T> {
     /**
-     * Returns an object whose properties have the value 'true'
+     * Is an object whose properties have the value 'true'
      * when they will be absent when used in a 'with' statement.
      */
-    [Symbol.unscopables](): {
-        copyWithin: boolean;
-        entries: boolean;
-        fill: boolean;
-        find: boolean;
-        findIndex: boolean;
-        keys: boolean;
-        values: boolean;
+    readonly [Symbol.unscopables]: {
+        [K in keyof any[]]?: boolean;
+    };
+}
+
+interface ReadonlyArray<T> {
+    /**
+     * Is an object whose properties have the value 'true'
+     * when they will be absent when used in a 'with' statement.
+     */
+    readonly [Symbol.unscopables]: {
+        [K in keyof readonly any[]]?: boolean;
     };
 }
 
